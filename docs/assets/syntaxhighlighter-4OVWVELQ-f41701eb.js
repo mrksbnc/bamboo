201dc0e30aadf7b2cdbb74f5815c55f6a019a304
Bence Márkus <markus.bence@icloud.com>
2023-08-19 00:07:44 +0200

    test 2

---
+++ docs/assets/syntaxhighlighter-4OVWVELQ-f41701eb.js
@@ -0,0 +1,2 @@
+import{S as c,c as l,s as n}from"./index-e3b0b9fd.js";import"./iframe-c905f399.js";import"../sb-preview/runtime.js";import"./index-d475d2ea.js";import"./_commonjsHelpers-725317a4.js";import"./index-d37d4223.js";import"./index-f073aaf1.js";import"./index-356e4a49.js";export{c as SyntaxHighlighter,l as createCopyToClipboardFunction,n as default};
+//# sourceMappingURL=syntaxhighlighter-4OVWVELQ-f41701eb.js.map
