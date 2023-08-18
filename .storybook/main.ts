c0b55cdd89d484163c6cd997c0b4abff3c077b3f
Bence Márkus <markus.bence@icloud.com>
2023-08-19 01:35:21 +0200

    docs changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/vue3-vite';
 
 export const config: StorybookConfig = {
-	stories: ['../src/docs/**/*.mdx', '../src/**/**/__stories__/*.stories.ts'],
+	stories: [
+		'../src/docs/**/*.mdx',
+		'../src/**/**/__stories__/*.mdx',
+		'../src/**/**/__stories__/*.stories.ts',
+	],
 	addons: [
 		'@storybook/addon-controls',
 		'@storybook/addon-a11y',
