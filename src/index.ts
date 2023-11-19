99e9d797add1942822c5369abe608094ef3dd5ea
Bence Markus <markus.bence@icloud.com>
2023-11-20 00:28:37 +0100

    artifact export

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
+export {
+	AvatarIndicatorPosition,
+	AvatarSize,
+	AvatarType,
+	AvatarVariant,
+	BoAvatar,
+	type AvatarIndicatorProps,
+	type AvatarInitialProps,
+} from '@/components/Avatar';
+export {
+	BadgeForm,
+	BadgeSize,
+	BadgeType,
+	BadgeVariant,
+	BoBadge,
+} from '@/components/Badge';
 export { BoIcon, Icon, IconSize } from '@/components/Icon';
 export { BambooColor } from '@/constants';
 export type { OptionalCss } from '@/types';
