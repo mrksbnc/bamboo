3945e3331a03e21b75184cc61c029b3a3d3ee6a1
Bence Markus <markus.bence@icloud.com>
2023-11-20 00:35:56 +0100

    github-flavored-markdown-mdx migration

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,14 +3,15 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 export const config: StorybookConfig = {
 	stories: ['./**/*.mdx', '../src/**/*.mdx', '../src/**/**/__stories__/*.stories.ts'],
 	addons: [
-		'storybook-dark-mode',
-		'@storybook/addon-a11y',
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-designs',
-		'@storybook/addon-themes',
-	],
+        'storybook-dark-mode',
+        '@storybook/addon-a11y',
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@storybook/addon-interactions',
+        '@storybook/addon-designs',
+        '@storybook/addon-themes',
+        '@storybook/addon-mdx-gfm'
+    ],
 	staticDirs: ['../src/assets'],
 	framework: {
 		name: '@storybook/vue3-vite',
