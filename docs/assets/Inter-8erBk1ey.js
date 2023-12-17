6cb769bb206def51cb150341296414b982016963
Bence Markus <markus.bence@icloud.com>
2023-12-17 21:59:13 +0100

    docs

---
+++ docs/assets/Inter-8erBk1ey.js
@@ -0,0 +1,17 @@
+import{j as t}from"./jsx-runtime-c1_SoqEi.js";import{M as x,T as s}from"./index-DBW6S8Xu.js";import{u as m}from"./index-1oRL8aVa.js";import"./iframe-_idAskDX.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-Uk8hDnuP.js";import"./_commonjsHelpers-5-cIlDoe.js";import"./index-ogXoivrg.js";import"./index-OpicemMp.js";import"./index-PPLHz8o0.js";const e={type:{primary:'"Inter" , sans-serif'},weight:{"font-light":300,"font-regular":400,"font-medium":500,"font-semibold":600,"font-bold":700,"font-extrabold":800},size:{"font-xxs":10,"font-xs":12,"font-sm":14,"font-md":16,"font-lg":18,"font-xl":20,"font-xxl":24,"font-xxxl":30}},i="Lorem ipsum dolor sit amet, consectetur adipiscing elit.";function r(o){const n=Object.assign({h1:"h1",p:"p",strong:"strong",h3:"h3"},m(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(x,{title:"Typeset/Inter"}),`
+`,`
+`,`
+`,t.jsx(n.h1,{id:"typography",children:"Typography"}),`
+`,t.jsxs(n.p,{children:[t.jsx(n.strong,{children:"Font:"})," Inter"]}),`
+`,t.jsxs(n.p,{children:[t.jsx(n.strong,{children:"Font sizes:"})," 10px(font-xxs), 12px(font-xs), 14px(font-sm), 16px(font-md), 18px(font-lg), 20px(font-xl), 24px(font-xxl), 30px(font-xxxl)"]}),`
+`,t.jsxs(n.p,{children:[t.jsx(n.strong,{children:"Font weights:"})," 300(font-light), 400(font-regular), 500(font-medium), 600(font-semibold), 700(font-bold), 800(font-extrabold)"]}),`
+`,t.jsx(n.h3,{id:"light",children:"Light"}),`
+`,t.jsx(s,{fontSizes:[Number(e.size["font-xxs"]),Number(e.size["font-xs"]),Number(e.size["font-sm"]),Number(e.size["font-md"]),Number(e.size["font-lg"]),Number(e.size["font-xl"]),Number(e.size["font-xxl"]),Number(e.size["font-xxxl"])],fontWeight:e.weight["font-light"],sampleText:i,fontFamily:e.type.primary}),`
+`,t.jsx(n.h3,{id:"regular",children:"Regular"}),`
+`,t.jsx(s,{fontSizes:[Number(e.size["font-xxs"]),Number(e.size["font-xs"]),Number(e.size["font-sm"]),Number(e.size["font-md"]),Number(e.size["font-lg"]),Number(e.size["font-xl"]),Number(e.size["font-xxl"]),Number(e.size["font-xxxl"])],fontWeight:e.weight["font-regular"],sampleText:i,fontFamily:e.type.primary}),`
+`,t.jsx(n.h3,{id:"medium",children:"Medium"}),`
+`,t.jsx(s,{fontSizes:[Number(e.size["font-xxs"]),Number(e.size["font-xs"]),Number(e.size["font-sm"]),Number(e.size["font-md"]),Number(e.size["font-lg"]),Number(e.size["font-xl"]),Number(e.size["font-xxl"]),Number(e.size["font-xxxl"])],fontWeight:e.weight["font-medium"],sampleText:i,fontFamily:e.type.primary}),`
+`,t.jsx(n.h3,{id:"semibold",children:"SemiBold"}),`
+`,t.jsx(s,{fontSizes:[Number(e.size["font-xxs"]),Number(e.size["font-xs"]),Number(e.size["font-sm"]),Number(e.size["font-md"]),Number(e.size["font-lg"]),Number(e.size["font-xl"]),Number(e.size["font-xxl"]),Number(e.size["font-xxxl"])],fontWeight:e.weight["font-semibold"],sampleText:i,fontFamily:e.type.primary}),`
+`,t.jsx(n.h3,{id:"bold",children:"Bold"}),`
+`,t.jsx(s,{fontSizes:[Number(e.size["font-xxs"]),Number(e.size["font-xs"]),Number(e.size["font-sm"]),Number(e.size["font-md"]),Number(e.size["font-lg"]),Number(e.size["font-xl"]),Number(e.size["font-xxl"]),Number(e.size["font-xxxl"])],fontWeight:e.weight["font-bold"],sampleText:i,fontFamily:e.type.primary})]})}function N(o={}){const{wrapper:n}=Object.assign({},m(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(r,o)})):r(o)}export{i as SampleText,N as default,e as typography};
--- storybook-static/assets/BoSpinner.stories-eNk6bFIy.js
+++
@@ -1,7 +0,0 @@
-import{a as d,L as a,S as t}from"./BoSpinner-sMiPDEWH.js";import{s as o}from"./storybook-j7faMZma.js";import"./vue.esm-bundler-ikl-1DwA.js";import"./_plugin-vue_export-helper-x3n3nnut.js";const S={title:"Loader/bo-spinner",component:d,tags:["autodocs"],argTypes:{size:{description:"The size of the spinner",table:{category:"props",subcategory:"optional",type:{summary:"LoaderSize",detail:o(a,"LoaderSize")}},control:{type:"select"},options:Object.values(a)},variant:{description:"The variant of the spinner",table:{category:"props",subcategory:"optional",type:{summary:"SpinnerVariant",detail:o(t,"SpinnerVariant")}},control:{type:"select"},options:Object.values(t)}}},e={args:{}},r={args:{text:"Loading..."}};var s,n,i;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
-  args: {}
-}`,...(i=(n=e.parameters)==null?void 0:n.docs)==null?void 0:i.source}}};var p,c,m;r.parameters={...r.parameters,docs:{...(p=r.parameters)==null?void 0:p.docs,source:{originalSource:`{
-  args: {
-    text: 'Loading...'
-  }
-}`,...(m=(c=r.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const x=["Example","WithText"];export{e as Example,r as WithText,x as __namedExportsOrder,S as default};
