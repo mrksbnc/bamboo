a685a353092555dffd702f65733ffb12f4a2684d
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2024-03-03 17:07:18 +0100

    bo button wip

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,11 +3,12 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-		'@storybook/addon-links',
-		'@storybook/addon-essentials',
-		'@chromatic-com/storybook',
-		'@storybook/addon-interactions',
-	],
+        '@storybook/addon-links',
+        '@storybook/addon-essentials',
+        '@chromatic-com/storybook',
+        '@storybook/addon-interactions',
+        '@storybook/addon-themes'
+    ],
 	framework: {
 		name: '@storybook/vue3-vite',
 		options: {},
---
+++ src/components/BoIcon/index.ts
@@ -0,0 +1,1 @@
+export { default as BoIcon } from '@/components/BoIcon/BoIcon.vue';
