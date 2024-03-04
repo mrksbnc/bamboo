6c007bac83393e233a3ee992817a84e4ac782773
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2024-03-04 21:42:42 +0100

    Update Storybook configuration and preview settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,13 @@ import type { StorybookConfig } from '@storybook/vue3-vite';
 const config: StorybookConfig = {
 	stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
 	addons: [
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@chromatic-com/storybook',
-        '@storybook/addon-interactions',
-        '@storybook/addon-themes'
-    ],
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		'@chromatic-com/storybook',
+		'@storybook/addon-interactions',
+		'@storybook/addon-themes',
+		'@storybook/theming',
+	],
 	framework: {
 		name: '@storybook/vue3-vite',
 		options: {},
