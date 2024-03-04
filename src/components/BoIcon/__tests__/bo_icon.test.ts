70411a61e8296bb10517bb6a007e1e2adf4a31a7
Bence Markus <markus.bence@icloud.com>
2024-03-04 22:49:03 +0100

    Icon tests added

---
+++ src/components/BoIcon/__tests__/bo_icon.test.ts
@@ -0,0 +1,27 @@
+import { BoSize } from '@/enums';
+import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { Icon } from '../ts/bo_icon';
+import BoIcon from '../vue/BoIcon.vue';
+
+let wrapper: ReturnType<typeof mount>;
+
+beforeEach(() => {
+	wrapper = mount(BoIcon, {
+		props: {
+			icon: Icon.activity,
+			size: BoSize.default,
+			color: '#000000',
+		},
+	});
+});
+
+describe('BoIcon', () => {
+	test('should match snapshot', () => {
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('should render the icon', () => {
+		expect(wrapper.find('activity')).toBeTruthy();
+	});
+});
