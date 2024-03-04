27234427795a736b66090150a75e75ff7d9068b8
Bence Markus <markus.bence@icloud.com>
2024-03-04 21:54:53 +0100

    bo-button tests added

---
+++ src/components/BoButton/__tests__/bo_button.test.ts
@@ -0,0 +1,55 @@
+import { BoSize } from '@/enums';
+import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { BoButtonType, BoButtonVariant } from '../ts/bo_button';
+import BoButton from '../vue/BoButton.vue';
+
+let wrapper: ReturnType<typeof mount>;
+
+beforeEach(() => {
+	wrapper = mount(BoButton, {
+		props: {
+			label: 'Label',
+			size: BoSize.default,
+			disabled: false,
+			type: BoButtonType.default,
+			variant: BoButtonVariant.primary,
+		},
+	});
+});
+
+describe('BoButton', () => {
+	test('should match snapshot', () => {
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('should render the label', () => {
+		expect(wrapper.text()).toBe('Label');
+	});
+
+	test('should have the default size', () => {
+		expect(wrapper.find('px-3')).toBeTruthy();
+		expect(wrapper.find('py-2')).toBeTruthy();
+	});
+
+	test('should have the default type', () => {
+		expect(wrapper.find('bg-blue-500')).toBeTruthy();
+		expect(wrapper.find('rounded-lg')).toBeTruthy();
+	});
+
+	test('should have the default text classes', () => {
+		expect(wrapper.find('text-white')).toBeTruthy();
+		expect(wrapper.find('text-sm')).toBeTruthy();
+	});
+
+	test('should be able to change the size', async () => {
+		await wrapper.setProps({ size: BoSize.large });
+		expect(wrapper.find('px-4')).toBeTruthy();
+		expect(wrapper.find('py-3')).toBeTruthy();
+	});
+
+	test('should be able to change the type', async () => {
+		await wrapper.setProps({ type: BoButtonType.outline });
+		expect(wrapper.find('border')).toBeTruthy();
+	});
+});
