a80e1e490a60f87afaa5ecf50ff1bd9f69c18ca0
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2024-03-05 19:28:39 +0100

    Refactor code for improved performance and readability

---
+++ src/components/BoButton/__tests__/bo_iocn_button.test.ts
@@ -0,0 +1,45 @@
+import { Icon } from '@/components/BoIcon';
+import { BoSize } from '@/constants';
+import { mount } from '@vue/test-utils';
+import { beforeEach, describe, expect, test } from 'vitest';
+import { BoIconButtonVariant } from '../ts/bo_button';
+import BoIconButton from '../vue/BoIconButton.vue';
+
+let wrapper: ReturnType<typeof mount>;
+
+beforeEach(() => {
+	wrapper = mount(BoIconButton, {
+		props: {
+			icon: Icon.activity,
+			size: BoSize.default,
+			disabled: false,
+			variant: BoIconButtonVariant.primary,
+		},
+	});
+});
+
+describe('BoIconButton', () => {
+	test('should match snapshot', () => {
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	test('should render the icon', () => {
+		expect(wrapper.find('svg')).toBeTruthy();
+	});
+
+	test('should have the default size', () => {
+		expect(wrapper.find('w-6')).toBeTruthy();
+		expect(wrapper.find('h-6')).toBeTruthy();
+	});
+
+	test('should have the default type', () => {
+		expect(wrapper.find('bg-blue-500')).toBeTruthy();
+		expect(wrapper.find('rounded-lg')).toBeTruthy();
+	});
+
+	test('should be able to change the size', async () => {
+		await wrapper.setProps({ size: BoSize.large });
+		expect(wrapper.find('w-8')).toBeTruthy();
+		expect(wrapper.find('h-8')).toBeTruthy();
+	});
+});
