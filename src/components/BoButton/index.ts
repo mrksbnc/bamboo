eec73c5bcaa54ed31fb0ec12d880da46822db1b4
Bence Markus <markus.bence@icloud.com>
2024-03-05 21:51:22 +0100

    Add ButtonGroupEvent and BoButtonGroupItem exports

--- src/components/BoButton/index.ts
+++ src/components/BoButton/index.ts
@@ -1,7 +1,8 @@
 export {
 	BoButtonType,
 	BoButtonVariant,
-	BoIconButtonVariant,
 } from '@/components/BoButton/ts/bo_button';
+export { ButtonGroupEvent } from '@/components/BoButton/ts/events';
+export type { BoButtonGroupItem } from '@/components/BoButton/ts/types';
 export { default as BoButton } from '@/components/BoButton/vue/BoButton.vue';
 export { default as BoIconButton } from '@/components/BoButton/vue/BoIconButton.vue';
