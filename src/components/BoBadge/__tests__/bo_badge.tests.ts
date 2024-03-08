2df9a445468e0bf79a30e690a93bffe135c75684
Bence Markus <markus.bence@icloud.com>
2024-03-08 23:53:08 +0100

    Add BoBadge component tests and BoButtonGroup component snapshot

---
+++ src/components/BoBadge/__tests__/bo_badge.tests.ts
@@ -0,0 +1,39 @@
+import { BoBadge } from '@/components/BoBadge';
+import { BoVariant } from '@/constants';
+import { mount } from '@vue/test-utils';
+import { describe, expect, it } from 'vitest';
+
+describe('BoBadge', () => {
+	it('should match the snapshot', () => {
+		const wrapper = mount(BoBadge, {
+			props: {
+				label: 'Badge',
+			},
+		});
+
+		expect(wrapper.html()).toMatchSnapshot();
+	});
+
+	it('should render the correct label', () => {
+		const label = 'Badge';
+		const wrapper = mount(BoBadge, {
+			props: {
+				label,
+			},
+		});
+
+		expect(wrapper.text()).toBe(label);
+	});
+
+	it('should render the correct variant', () => {
+		const variant = 'success';
+		const wrapper = mount(BoBadge, {
+			props: {
+				label: 'Badge',
+				variant: BoVariant.success,
+			},
+		});
+
+		expect(wrapper.classes()).toContain(`bo-badge--${variant}`);
+	});
+});
