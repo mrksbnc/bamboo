890abeb715ff716aa343ee11093dad31d777de84
Bence Markus <markus.bence@icloud.com>
2024-03-11 02:08:04 +0100

    Add type definition for CssClassMap

--- src/types/css_class_map.ts
+++ src/types/css_class_map.ts
@@ -1,1 +1,6 @@
+/**
+ * An object that maps class names to their respective values.
+ *
+ * @template T - The type of keys in the map. `T extends string` ensures that the keys are strings.
+ */
 export declare type CssClassMap<T extends string> = Readonly<Record<T, string>>;
