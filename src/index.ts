6662e25cffbee271311a55a71f0442f3a4017920
Bence Markus <markus.bence@icloud.com>
2024-03-13 19:03:51 +0100

    Remove unused components and add new components

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,6 @@
-export * from './components';
+export * from '@/components/BoBadge';
+export * from '@/components/BoButton';
+export * from '@/components/BoButtonGroup';
+export * from '@/components/BoIcon';
+export * from '@/components/BoIconButton';
 export * from './constants';
