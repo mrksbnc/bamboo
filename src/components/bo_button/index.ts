24475c36935e71cc0a6d7cd70ac797b3d85c7548
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2024-06-15 20:19:04 +0200

    bo-button rework | new classes and style fixes

---
+++ src/components/bo_button/index.ts
@@ -0,0 +1,3 @@
+export { default as BoButton } from './bo-button.vue';
+export { BoButtonType, BoButtonVariant } from './bo_button.constants';
+export type { BoButtonIconArgs } from './bo_button.types';
