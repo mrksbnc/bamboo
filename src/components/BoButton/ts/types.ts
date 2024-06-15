e6f6bb444a571dc3c5a0073acb713b47e6ed6eba
Bence Markus <markus.bence@icloud.com>
2024-06-15 21:03:06 +0200

    old code removed

--- src/components/BoButton/ts/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Icon } from '@/components/BoIcon';
-
-export declare type BoButtonGroupItem = {
-	id: string | number;
-	label: string;
-	disabled?: boolean;
-	prefixIcon?: Icon;
-	suffixIcon?: Icon;
-};
--- src/global/index.ts
+++ src/global/index.ts
@@ -1,3 +1,2 @@
 export { BoColor } from './bo_color';
 export { BoSize } from './bo_size';
-export { BoVariant } from './bo_variant';
