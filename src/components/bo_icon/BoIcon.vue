f73d1e700ac22b0d2615bf1ae83ceff1292f13c0
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2024-06-15 21:00:52 +0200

    refactor | component changes and new mdx based doc for the component

---
+++ src/components/bo_icon/BoIcon.vue
@@ -0,0 +1,83 @@
+<template>
+	<div
+		v-html="svg"
+		:style="style"
+		class="bo-icon"
+		:class="classes"
+		aria-label="icon"
+		type="image/svg+xml"
+	/>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+	inheritAttrs: true,
+});
+</script>
+
+<script setup lang="ts">
+import { IconSizeClasses, icons, type Icon } from './bo_icon';
+import { BoSize } from '@/global';
+import {
+	ref,
+	toRefs,
+	watch,
+	type PropType,
+	defineComponent,
+	computed,
+	type StyleValue,
+} from 'vue';
+
+const props = defineProps({
+	icon: {
+		type: String as PropType<Icon>,
+		required: true,
+	},
+	size: {
+		type: String as PropType<BoSize>,
+		default: () => BoSize.default,
+	},
+	color: {
+		type: String,
+		default: () => 'currentColor',
+	},
+});
+
+const { icon, size, color } = toRefs(props);
+
+const svg = ref('');
+
+const iconMap = Object.keys(icons).reduce(
+	(acc, key) => {
+		const splitted = key.split('/');
+		const icon = splitted[splitted.length - 1].split('.')[0];
+
+		acc[icon] = icons[key];
+
+		return acc;
+	},
+	{} as Record<string, () => Promise<string>>,
+);
+
+const style = computed<StyleValue>(() => {
+	return {
+		color: color.value,
+	};
+});
+
+const classes = computed<string>(() => {
+	return IconSizeClasses[size.value];
+});
+
+const load = async (): Promise<void> => {
+	try {
+		iconMap[icon.value]().then((val) => {
+			svg.value = val;
+		});
+	} catch (e) {
+		console.error(`Could not find icon of name ${icon.value}`);
+	}
+};
+
+watch(icon, () => load(), { immediate: true });
+</script>
---
+++ src/components/bo_icon/index.ts
@@ -0,0 +1,2 @@
+export { default as BoIcon } from './BoIcon.vue';
+export { Icon, icons } from './bo_icon';
