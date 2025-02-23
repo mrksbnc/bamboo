4008feee8497b6b84c766b5160338ddc68eb120f
Bence Markus <markusbence@outlook.com>
2025-02-23 16:14:46 +0100

    test: new bo badge tersts added and old incorrect test logic fixed/removed

--- tests/components/bo_badge.test.ts
+++ tests/components/bo_badge.test.ts
@@ -1,6 +1,7 @@
 import { BoBadge, BoBadgeShape, BoBadgeType, BoBadgeVariant } from '@/components/bo_badge';
 import { BoIcon, Icon } from '@/components/bo_icon';
 import BoText from '@/components/bo_text/BoText.vue';
+import { BoSize } from '@/shared';
 import { mount } from '@vue/test-utils';
 import { beforeEach, describe, expect, suite, test } from 'vitest';
 
@@ -20,14 +21,15 @@ beforeEach(() => {
 describe('bo_badge.vue', () => {
 	test('the badge should be rendered', () => {
 		expect(wrapper.find('span')).toBeTruthy();
+		expect(wrapper.find('span').classes()).contains('bo-badge');
 	});
 
 	test('the badge container should have the default classes', () => {
-		expect(wrapper.find('.bo-badge').classes()).contains('inline-flex');
-		expect(wrapper.find('.bo-badge').classes()).contains('items-center');
-		expect(wrapper.find('.bo-badge').classes()).contains('justify-center');
-		expect(wrapper.find('.bo-badge').classes()).contains('select-none');
-		expect(wrapper.find('.bo-badge').classes()).contains('max-h-fit');
+		expect(wrapper.classes()).contains('inline-flex');
+		expect(wrapper.classes()).contains('items-center');
+		expect(wrapper.classes()).contains('justify-center');
+		expect(wrapper.classes()).contains('select-none');
+		expect(wrapper.classes()).contains('max-h-fit');
 	});
 
 	test('the badge should render the label', () => {
@@ -37,90 +39,283 @@ describe('bo_badge.vue', () => {
 	suite('badge types', () => {
 		test('the badge should have the default type', async () => {
 			await wrapper.setProps({ type: BoBadgeType.default });
-			expect(wrapper.find('bg-blue-500')).toBeTruthy();
+			expect(wrapper.classes()).contains('bg-blue-100');
+			expect(wrapper.classes()).contains('text-blue-800');
+			expect(wrapper.classes()).contains('border');
+			expect(wrapper.classes()).contains('border-blue-200');
 		});
 
 		test('the badge should support outline type', async () => {
 			await wrapper.setProps({ type: BoBadgeType.outline });
-			expect(wrapper.find('border')).toBeTruthy();
+			expect(wrapper.classes()).contains('border');
+			expect(wrapper.classes()).contains('border-blue-600');
 		});
 	});
 
 	suite('badge sizes', () => {
-		test('the badge should have the default size', () => {
-			expect(wrapper.find('w-6')).toBeTruthy();
-			expect(wrapper.find('h-6')).toBeTruthy();
+		suite('default', () => {
+			const testWrapper = mount(BoBadge, {
+				props: {
+					label: 'Label',
+					type: BoBadgeType.default,
+					variant: BoBadgeVariant.primary,
+				},
+			});
+
+			test('the badge should support extra small size related classes', async () => {
+				await testWrapper.setProps({ size: BoSize.extra_small });
+				expect(testWrapper.classes()).contains('px-1.5');
+				expect(testWrapper.classes()).contains('py-0');
+			});
+
+			test('the badge should support small size related classes', async () => {
+				await testWrapper.setProps({ size: BoSize.small });
+				expect(testWrapper.classes()).contains('px-2');
+				expect(testWrapper.classes()).contains('py-0.5');
+			});
+
+			test('the badge should have the default size related classes', async () => {
+				await testWrapper.setProps({ size: BoSize.default });
+				expect(testWrapper.classes()).contains('px-2.5');
+				expect(testWrapper.classes()).contains('py-1');
+			});
+
+			test('the badge should support large size related classes', async () => {
+				await testWrapper.setProps({ size: BoSize.large });
+				expect(testWrapper.classes()).contains('px-3');
+				expect(testWrapper.classes()).contains('py-1.5');
+			});
+
+			test('the badge should support default size related classes', async () => {
+				await testWrapper.setProps({ size: BoSize.extra_large });
+				expect(testWrapper.classes()).contains('px-3.5');
+				expect(testWrapper.classes()).contains('py-2.5');
+			});
 		});
 
-		test('the badge should support extra small size', () => {
-			expect(wrapper.find('w-4')).toBeTruthy();
-			expect(wrapper.find('h-4')).toBeTruthy();
-		});
+		suite('icon only', () => {
+			suite('default', () => {
+				const testWrapper = mount(BoBadge, {
+					props: {
+						prefixIcon: Icon.activity,
+						type: BoBadgeType.default,
+						variant: BoBadgeVariant.primary,
+					},
+				});
 
-		test('the badge should support small size', () => {
-			expect(wrapper.find('w-8')).toBeTruthy();
-			expect(wrapper.find('h-8')).toBeTruthy();
-		});
+				test('the badge should support extra small size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.extra_small });
+					expect(testWrapper.classes()).contains('px-1.5');
+					expect(testWrapper.classes()).contains('py-0');
+				});
 
-		test('the badge should support large size', () => {
-			expect(wrapper.find('w-12')).toBeTruthy();
-			expect(wrapper.find('h-12')).toBeTruthy();
-		});
+				test('the badge should support small size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.small });
+					expect(testWrapper.classes()).contains('px-2');
+					expect(testWrapper.classes()).contains('py-0.5');
+				});
 
-		test('the badge should support extra large size', () => {
-			expect(wrapper.find('w-16')).toBeTruthy();
-			expect(wrapper.find('h-16')).toBeTruthy();
+				test('the badge should have the default size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.default });
+					expect(testWrapper.classes()).contains('px-2.5');
+					expect(testWrapper.classes()).contains('py-1');
+				});
+
+				test('the badge should support large size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.large });
+					expect(testWrapper.classes()).contains('px-3');
+					expect(testWrapper.classes()).contains('py-1.5');
+				});
+
+				test('the badge should support default size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.extra_large });
+					expect(testWrapper.classes()).contains('px-3.5');
+					expect(testWrapper.classes()).contains('py-2.5');
+				});
+			});
+
+			suite('circle', () => {
+				const testWrapper = mount(BoBadge, {
+					props: {
+						prefixIcon: Icon.activity,
+						shape: BoBadgeShape.circle,
+						type: BoBadgeType.default,
+						variant: BoBadgeVariant.primary,
+					},
+				});
+
+				test('the badge should support extra small size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.extra_small });
+					expect(testWrapper.classes()).contains('p-0');
+				});
+
+				test('the badge should support small size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.small });
+					expect(testWrapper.classes()).contains('p-0.5');
+				});
+
+				test('the badge should have the default size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.default });
+					expect(testWrapper.classes()).contains('p-1');
+				});
+
+				test('the badge should support large size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.large });
+					expect(testWrapper.classes()).contains('p-1.5');
+				});
+
+				test('the badge should support default size related classes', async () => {
+					await testWrapper.setProps({ size: BoSize.extra_large });
+					expect(testWrapper.classes()).contains('p-2.5');
+				});
+			});
 		});
 	});
 
 	suite('badge shapes', () => {
 		test('the badge should have the default shape', () => {
-			expect(wrapper.find('rounded-lg')).toBeTruthy();
+			expect(wrapper.classes()).contains('rounded-md');
 		});
 
 		test('the badge should support circle shape', async () => {
 			await wrapper.setProps({ shape: BoBadgeShape.circle });
-			expect(wrapper.find('rounded-full')).toBeTruthy();
+			expect(wrapper.classes()).contains('rounded-full');
 		});
 
 		test('the badge should support pill shape', async () => {
 			await wrapper.setProps({ shape: BoBadgeShape.pill });
-			expect(wrapper.find('rounded-full')).toBeTruthy();
+			expect(wrapper.classes()).contains('rounded-full');
 		});
 
 		test('the badge should support flat shape', async () => {
 			await wrapper.setProps({ shape: BoBadgeShape.flat });
-			expect(wrapper.find('rounded-none')).toBeTruthy();
+			expect(wrapper.classes()).contains('rounded-none');
 		});
 	});
 
 	suite('badge variants', () => {
-		test('the badge should have the default variant', () => {
-			expect(wrapper.find('bg-blue-500')).toBeTruthy();
+		suite('default', () => {
+			const testWrapper = mount(BoBadge, {
+				props: {
+					label: 'Label',
+					type: BoBadgeType.default,
+					variant: BoBadgeVariant.primary,
+				},
+			});
+
+			test('the badge should have the default variant', async () => {
+				expect(testWrapper.classes()).contains('bg-blue-100');
+				expect(testWrapper.classes()).contains('text-blue-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-blue-200');
+			});
+
+			test('the badge should support primary variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.primary });
+				expect(testWrapper.classes()).contains('bg-blue-100');
+				expect(testWrapper.classes()).contains('text-blue-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-blue-200');
+			});
+
+			test('the badge should support secondary variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.secondary });
+				expect(testWrapper.classes()).contains('bg-gray-100');
+				expect(testWrapper.classes()).contains('text-gray-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-gray-200');
+			});
+
+			test('the badge should support danger variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.danger });
+				expect(testWrapper.classes()).contains('bg-red-100');
+				expect(testWrapper.classes()).contains('text-red-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-red-200');
+			});
+
+			test('the badge should support warning variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.warning });
+				expect(testWrapper.classes()).contains('bg-yellow-100');
+				expect(testWrapper.classes()).contains('text-yellow-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-yellow-200');
+			});
+
+			test('the badge should support success variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.success });
+				expect(testWrapper.classes()).contains('bg-green-100');
+				expect(testWrapper.classes()).contains('text-green-800');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-green-200');
+			});
+
+			test('the badge should support dark variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.dark });
+				expect(testWrapper.classes()).contains('bg-black');
+				expect(testWrapper.classes()).contains('text-white');
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-black');
+			});
 		});
 
-		test('the badge should support primary variant', () => {
-			expect(wrapper.find('bg-blue-600')).toBeTruthy();
-		});
+		suite('outline', () => {
+			const testWrapper = mount(BoBadge, {
+				props: {
+					label: 'Label',
+					type: BoBadgeType.outline,
+					variant: BoBadgeVariant.primary,
+				},
+			});
 
-		test('the badge should support secondary variant', () => {
-			expect(wrapper.find('bg-gray-600')).toBeTruthy();
-		});
+			test('the badge should have the default variant', () => {
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-blue-600');
+				expect(testWrapper.classes()).contains('text-blue-600');
+			});
 
-		test('the badge should support danger variant', () => {
-			expect(wrapper.find('bg-red-600')).toBeTruthy();
-		});
+			test('the badge should support primary variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.primary });
 
-		test('the badge should support warning variant', () => {
-			expect(wrapper.find('bg-yellow-600')).toBeTruthy();
-		});
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-blue-600');
+				expect(testWrapper.classes()).contains('text-blue-600');
+			});
 
-		test('the badge should support success variant', () => {
-			expect(wrapper.find('bg-green-600')).toBeTruthy();
-		});
+			test('the badge should support secondary variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.secondary });
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-gray-600');
+				expect(testWrapper.classes()).contains('text-gray-600');
+			});
 
-		test('the badge should support dark variant', () => {
-			expect(wrapper.find('bg-black')).toBeTruthy();
+			test('the badge should support danger variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.danger });
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-red-600');
+				expect(testWrapper.classes()).contains('text-red-600');
+			});
+
+			test('the badge should support warning variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.warning });
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-yellow-600');
+				expect(testWrapper.classes()).contains('text-yellow-600');
+			});
+
+			test('the badge should support success variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.success });
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-green-600');
+				expect(testWrapper.classes()).contains('text-green-600');
+			});
+
+			test('the badge should support dark variant', async () => {
+				await testWrapper.setProps({ variant: BoBadgeVariant.dark });
+				expect(testWrapper.classes()).contains('border');
+				expect(testWrapper.classes()).contains('border-black');
+				expect(testWrapper.classes()).contains('text-black');
+			});
 		});
 	});
 
@@ -137,14 +332,6 @@ describe('bo_badge.vue', () => {
 
 		test('the badge should be able to display a suffix icon', async () => {
 			await wrapper.setProps({ suffixIcon: Icon.activity });
-			expect(wrapper.findComponent(BoIcon).exists()).toBe(true);
-		});
-
-		test('the badge should be able to display both prefix and suffix icons', async () => {
-			await wrapper.setProps({
-				prefixIcon: Icon.activity,
-				suffixIcon: Icon.airplay,
-			});
 			expect(wrapper.findComponent(BoIcon).exists()).toBe(true);
 		});
 	});
