0120d75c3084c13ef03e8fffc26af92ec46caaf8
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2025-10-19 16:59:12 +0200

    test: new loader tests added

--- src-docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
+++ src-docs/.vitepress/cache/deps/vitepress___@vueuse_core.js
@@ -38,7 +38,7 @@ import {
   watchEffect
 } from "./chunk-I7HJNI4K.js";
 
-// node_modules/.pnpm/@vueuse+shared@13.9.0_vue@3.5.21_typescript@5.8.3_/node_modules/@vueuse/shared/index.mjs
+// node_modules/.pnpm/@vueuse+shared@13.9.0_vue@3.5.22_typescript@5.9.3_/node_modules/@vueuse/shared/index.mjs
 function computedEager(fn, options) {
   var _a;
   const result = shallowRef();
@@ -1572,7 +1572,7 @@ function whenever(source, cb, options) {
   return stop;
 }
 
-// node_modules/.pnpm/@vueuse+core@13.9.0_vue@3.5.21_typescript@5.8.3_/node_modules/@vueuse/core/index.mjs
+// node_modules/.pnpm/@vueuse+core@13.9.0_vue@3.5.22_typescript@5.9.3_/node_modules/@vueuse/core/index.mjs
 function computedAsync(evaluationCallback, initialState, optionsOrRef) {
   var _a;
   let options;
