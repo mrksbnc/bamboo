dcfedea7733fafaaccd378b76caddfbe2245c65f
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2025-11-09 12:21:55 +0100

    chore: husky update

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,41 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import type { Theme } from 'vitepress'
-import DefaultTheme from 'vitepress/theme'
-import './style.css'
+import { h, onMounted, watch } from 'vue';
+import type { Theme } from 'vitepress';
+import DefaultTheme from 'vitepress/theme';
+import { useData } from 'vitepress';
+import '@/lib.css';
+import BoTooltipContainer from '@/components/bo-tooltip/bo-tooltip-container.vue';
 
 export default {
 	extends: DefaultTheme,
 	Layout: () => {
 		return h(DefaultTheme.Layout, null, {
 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
-		})
+			'layout-bottom': () => h(BoTooltipContainer),
+		});
 	},
 	// oxlint-disable-next-line no-unused-vars
 	enhanceApp({ app, router, siteData }) {
-		// ...
+		if (typeof window !== 'undefined') {
+			app.mixin({
+				setup() {
+					const { isDark } = useData();
+
+					const updateTheme = () => {
+						const html = document.documentElement;
+						if (isDark.value) {
+							html.setAttribute('data-theme', 'dark');
+						} else {
+							html.setAttribute('data-theme', 'light');
+						}
+					};
+
+					onMounted(() => {
+						updateTheme();
+						watch(isDark, updateTheme);
+					});
+				},
+			});
+		}
 	},
-} satisfies Theme
+} satisfies Theme;
