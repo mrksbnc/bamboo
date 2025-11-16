fa7898d4884c0114e3e5892894f8d1c23c0320cf
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2025-11-16 16:17:57 +0100

    import cleanup

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -4,14 +4,12 @@ import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
 import { useData } from 'vitepress';
 import '@/lib.css';
-import BoTooltipContainer from '@/components/bo-tooltip/bo-tooltip-container.vue';
 
 export default {
 	extends: DefaultTheme,
 	Layout: () => {
 		return h(DefaultTheme.Layout, null, {
 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
-			'layout-bottom': () => h(BoTooltipContainer),
 		});
 	},
 	// oxlint-disable-next-line no-unused-vars
