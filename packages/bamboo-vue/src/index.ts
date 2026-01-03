b01f3e9db6e2810a89b9b89815319180fc7a52b1
Turbobot <turbobot@vercel.com>
2026-01-03 21:57:06 +0100

    feat(create-turbo): apply official-starter transform

--- packages/bamboo-vue/src/index.ts
+++ packages/bamboo-vue/src/index.ts
@@ -1,3 +1,9 @@
+/** Force vite to bundle the base css with the component styles */
 import './lib.css';
 
-export * from './components/index.js';
+/** Component exports */
+export * from './components/bo-button/index.js';
+export * from './components/bo-loading-ring/index.js';
+export * from './components/bo-loading-spinner/index.js';
+export * from './components/bo-icon/index.js';
+export * from './components/bo-text/index.js';
