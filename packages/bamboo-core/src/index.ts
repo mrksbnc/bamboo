0d638d72d165cd023f5b4daab4b8b2b93ad27cea
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-01-04 23:32:00 +0100

    wip

--- packages/bamboo-core/src/index.ts
+++ packages/bamboo-core/src/index.ts
@@ -1,16 +1,25 @@
-/** Constants */
+/** Components */
 
-export { AriaLive } from './constants/bo-accessibility.js';
-export { BoLoaderTextPosition, BoLoaderType } from './constants/bo-loader.js';
-export { BoButtonVariant } from './constants/bo-variants.js';
-export { BoSize } from './constants/bo-size.js';
-export { BoLocalStorageKey } from './constants/bo-local-storage.js';
-export type { Booleanish, ConditionalCssProperties } from './constants/bo-types.js';
-export { iconRegistry } from './constants/bo-svg.js';
-export { Icon } from './constants/bo-icon.js';
+export {
+	type BoButtonSize,
+	type BoButtonStyle,
+	type BoButtonVariant,
+	BoButtonVariantStyle,
+	type BoVariantStyle,
+} from './components/bo-button.js';
+export { Icon } from './components/bo-icon.js';
+export { ICON_REGISTRY, type IconRegistry } from './components/bo-icon-registry.js';
+export { type BoLoaderTextPosition, type BoLoaderType } from './components/bo-loader.js';
+export { type BoInputSize, type BoInputType, BoInputSizeStyle } from './components/bo-input.js';
 
 /** Services */
 
 export { ColorService } from './services/color-service.js';
 export { IdentityService } from './services/identity-service.js';
 export { Theme, ThemeService } from './services/theme-service.js';
+
+/** Shared constants and types */
+
+export { AriaLive } from './bo-accessibility.js';
+export { BoLocalStorageKey } from './bo-local-storage.js';
+export type { Booleanish, ConditionalCssProperties } from './common-types.js';
