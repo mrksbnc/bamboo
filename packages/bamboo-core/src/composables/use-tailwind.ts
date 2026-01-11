b8d5f50aa3489f85e9bf5af51706710c1b476a03
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-01-11 21:12:25 +0100

    fix: tw merge logic optimisation

--- packages/bamboo-core/src/composables/use-tailwind.ts
+++ packages/bamboo-core/src/composables/use-tailwind.ts
@@ -3,16 +3,16 @@ export interface UseTailwind {
 }
 
 export const useTailwind = (): UseTailwind => {
-	function merge(...classes: string[]): string {
+	const merge = (...classes: string[]): string => {
 		return Array.from(
 			new Set<string>(
 				classes
 					.map((c) => c?.split(' ') ?? [])
-					.flatMap((c) => c)
+					.flat()
 					.filter(Boolean),
 			).values(),
 		).join(' ');
-	}
+	};
 
 	return {
 		merge,
