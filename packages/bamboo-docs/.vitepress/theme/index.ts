c1da64c6b938a9933bb3505a50e061338a4c65a0
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-01-17 00:26:30 +0100

    wip

--- packages/bamboo-docs/.vitepress/theme/index.ts
+++ packages/bamboo-docs/.vitepress/theme/index.ts
@@ -1,13 +1,11 @@
 // https://vitepress.dev/guide/custom-theme
-import { h, onMounted, watch } from 'vue';
+import { h } from 'vue';
 import type { Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
-import { useData } from 'vitepress';
 
-import { BoText, BoButton } from '@mrksbnc/bamboo-vue';
+import './style.css';
 
 import '@mrksbnc/bamboo-vue/lib.css';
-import './style.css';
 
 export default {
 	extends: DefaultTheme,
@@ -16,30 +14,7 @@ export default {
 			// https://vitepress.dev/guide/extending-default-theme#layout-slots
 		});
 	},
-
-	enhanceApp({ app }) {
-		if (typeof window !== 'undefined') {
-			app.component('BoText', BoText);
-			app.component('BoButton', BoButton);
-			app.mixin({
-				setup() {
-					const { isDark } = useData();
-
-					const updateTheme = (): void => {
-						const html = document.documentElement;
-						if (isDark.value) {
-							html.setAttribute('data-theme', 'dark');
-						} else {
-							html.setAttribute('data-theme', 'light');
-						}
-					};
-
-					onMounted(() => {
-						updateTheme();
-						watch(isDark, updateTheme);
-					});
-				},
-			});
-		}
+	enhanceApp() {
+		// ...
 	},
 } satisfies Theme;
