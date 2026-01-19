373f63f170ccd1b864340af1c0bf6e3c03383aba
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-01-20 00:19:56 +0100

    feat: docs with localisation

---
+++ packages/bamboo-vue/src/plugin.ts
@@ -0,0 +1,32 @@
+import type { App, ObjectPlugin } from 'vue';
+import {
+	BoAvatar,
+	BoBadge,
+	BoButton,
+	BoIcon,
+	BoInput,
+	BoLoadingPulse,
+	BoLoadingSpinner,
+	BoText,
+} from './components/index.js';
+
+const BambooComponents = {
+	BoAvatar,
+	BoBadge,
+	BoButton,
+	BoIcon,
+	BoInput,
+	BoLoadingPulse,
+	BoLoadingSpinner,
+	BoText,
+};
+
+export const BambooVue = {
+	install(app: App): void {
+		for (const [name, component] of Object.entries(BambooComponents)) {
+			if (name.startsWith('Bo') && typeof component === 'object') {
+				app.component(name, component);
+			}
+		}
+	},
+} as const satisfies ObjectPlugin;
