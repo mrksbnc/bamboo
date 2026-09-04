f6b671de4504032fe46553119d993902018bf971
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-09-04 15:38:46 +0200

    feat(docs): add syntax highlighting to example frame

--- packages/bamboo-docs/.vitepress/theme/components/ExampleFrame.vue
+++ packages/bamboo-docs/.vitepress/theme/components/ExampleFrame.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { computed, ref, useId } from 'vue';
+import { codeToHtml, type BundledLanguage } from 'shiki';
+import { ref, useId, watch } from 'vue';
 
 const props = withDefaults(
 	defineProps<{
@@ -15,7 +16,38 @@ const props = withDefaults(
 
 const isCodeVisible = ref(false);
 const codeId = `example-code-${useId()}`;
-const codeLines = computed<string[]>(() => props.code.trim().split(/\r?\n/));
+
+function escapeHtml(value: string): string {
+	return value
+		.replaceAll('&', '&amp;')
+		.replaceAll('<', '&lt;')
+		.replaceAll('>', '&gt;')
+		.replaceAll('"', '&quot;')
+		.replaceAll("'", '&#039;');
+}
+
+async function highlightCode(code: string, lang: string): Promise<string> {
+	try {
+		return await codeToHtml(code, {
+			lang: lang as BundledLanguage,
+			themes: {
+				light: 'github-light',
+				dark: 'github-dark',
+			},
+		});
+	} catch {
+		return `<pre><code>${escapeHtml(code)}</code></pre>`;
+	}
+}
+
+const highlightedCode = ref(await highlightCode(props.code.trim(), props.lang));
+
+watch(
+	() => [props.code, props.lang],
+	async ([code, lang]) => {
+		highlightedCode.value = await highlightCode(code.trim(), lang);
+	},
+);
 </script>
 
 <template>
@@ -23,12 +55,14 @@ const codeLines = computed<string[]>(() => props.code.trim().split(/\r?\n/));
 		<div class="example-frame-preview" data-slot="preview" :data-align="align">
 			<slot />
 		</div>
-		<div class="example-frame-code" data-slot="code" :data-expanded="isCodeVisible">
-			<pre :id="codeId" :data-lang="lang" tabindex="0"><code><span
-				v-for="(line, index) in codeLines"
-				:key="`${index}-${line}`"
-				class="example-frame-code-line"
-			><span class="example-frame-line-number" aria-hidden="true">{{ index + 1 }}</span><span>{{ line || ' ' }}</span></span></code></pre>
+		<div
+			:id="codeId"
+			class="example-frame-code"
+			data-slot="code"
+			:data-lang="lang"
+			:data-expanded="isCodeVisible"
+		>
+			<div class="example-frame-code-content" v-html="highlightedCode"></div>
 			<div v-if="!isCodeVisible" class="example-frame-code-fade">
 				<button
 					type="button"
