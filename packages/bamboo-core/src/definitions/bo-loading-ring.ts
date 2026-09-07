5ba80bad5c01840b76cd1c3d8927a2eca1bc2eb5
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-09-07 21:31:01 +0200

    feat(core): add loading ring definition

---
+++ packages/bamboo-core/src/definitions/bo-loading-ring.ts
@@ -0,0 +1,6 @@
+import type { BoLoadingSpinnerProps } from './bo-loading-spinner.js';
+
+export interface BoLoadingRingProps extends BoLoadingSpinnerProps {
+	customContainerCssClass?: string;
+	customRingClass?: string;
+}
