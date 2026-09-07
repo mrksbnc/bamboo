537aea88e008a8da81c4373e1f5736b3505db083
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-09-07 21:32:47 +0200

    style(core): align switch manifest

--- packages/bamboo-core/src/manifests/switch.manifest.ts
+++ packages/bamboo-core/src/manifests/switch.manifest.ts
@@ -4,19 +4,11 @@ export const SWITCH_MANIFEST = {
 	meta: { name: 'Switch', version: '1.0.0' },
 	styles: {
 		base: 'bo-switch',
-		control: {
-			sm: 'bo-switch__control--sm',
-			default: 'bo-switch__control--default',
-			lg: 'bo-switch__control--lg',
-		},
-		thumb: {
-			sm: 'bo-switch__thumb--sm',
-			default: 'bo-switch__thumb--default',
-			lg: 'bo-switch__thumb--lg',
-		},
+		control: 'bo-switch__control',
+		thumb: 'bo-switch__thumb',
 		labelGroup: 'bo-switch__label-group',
 		label: 'bo-switch__label',
 		description: 'bo-switch__description',
 	},
-	defaults: { size: 'default', orientation: 'horizontal', role: 'switch' },
+	defaults: { orientation: 'horizontal', role: 'switch' },
 } as const satisfies SwitchManifest;
