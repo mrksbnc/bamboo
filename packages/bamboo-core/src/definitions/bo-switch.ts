3602f9f6fce53e5fd92fb06db371b2ee85a12d94
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-09-07 21:31:34 +0200

    fix(core): align switch definition

--- packages/bamboo-core/src/definitions/bo-switch.ts
+++ packages/bamboo-core/src/definitions/bo-switch.ts
@@ -1,14 +1,24 @@
 import { HTMLAttributes } from 'vue';
 import { ComponentManifest } from '../manifests/types.js';
 
-export type BoSwitchSize = 'sm' | 'default' | 'lg';
 export type BoSwitchOrientation = 'horizontal' | 'vertical';
+
+export interface BoSwitchCustomColors {
+	background?: string;
+	checkedBackground?: string;
+	border?: string;
+	thumb?: string;
+	checkedThumb?: string;
+	text?: string;
+}
+
+export type BoSwitchCustomColor = string | BoSwitchCustomColors;
 
 export interface BoSwitchProps {
 	id?: HTMLElement['id'];
 	dataTestId?: string;
 	modelValue?: boolean;
-	size?: BoSwitchSize;
+	defaultValue?: boolean;
 	orientation?: BoSwitchOrientation;
 	label?: string;
 	description?: string;
@@ -16,18 +26,20 @@ export interface BoSwitchProps {
 	required?: boolean;
 	name?: string;
 	value?: string;
+	customColor?: BoSwitchCustomColor;
+	customThumbColor?: string;
 	role?: HTMLAttributes['role'];
 	ariaLabel?: HTMLAttributes['aria-label'];
 	ariaLabelledBy?: HTMLAttributes['aria-labelledby'];
 	ariaDescribedBy?: HTMLAttributes['aria-describedby'];
 }
 
-type BoSwitchManifestDefaults = Pick<BoSwitchProps, 'size' | 'orientation' | 'role'>;
+type BoSwitchManifestDefaults = Pick<BoSwitchProps, 'orientation' | 'role'>;
 
 export interface BoSwitchStyleManifest {
 	base: string;
-	control: Record<BoSwitchSize, string>;
-	thumb: Record<BoSwitchSize, string>;
+	control: string;
+	thumb: string;
 	labelGroup: string;
 	label: string;
 	description: string;
