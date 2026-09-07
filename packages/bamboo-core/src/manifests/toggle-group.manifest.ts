16c661af05c681c816a48cd6e06dd07a0117f804
Bence Markus <23279421+mrksbnc@users.noreply.github.com>
2026-09-07 21:32:56 +0200

    style(core): align toggle group manifest

--- packages/bamboo-core/src/manifests/toggle-group.manifest.ts
+++ packages/bamboo-core/src/manifests/toggle-group.manifest.ts
@@ -9,6 +9,16 @@ export const TOGGLE_GROUP_MANIFEST = {
 			vertical: 'bo-toggle-group--vertical',
 		},
 		item: 'bo-toggle-group__item',
+		variant: {
+			default: 'bo-toggle-group__item--default',
+			outline: 'bo-toggle-group__item--outline',
+		},
+		size: {
+			sm: 'bo-toggle-group__item--sm',
+			default: 'bo-toggle-group__item--default-size',
+			lg: 'bo-toggle-group__item--lg',
+		},
+		selected: 'bo-toggle-group__item--selected',
 	},
 	defaults: { variant: 'default', size: 'default', orientation: 'horizontal', role: 'group' },
 } as const satisfies ToggleGroupManifest;
